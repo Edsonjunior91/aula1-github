fc52181b60bf2583246e16f6f489484bb80398d0
Edson Junior <edson_junior91@hotmail.com>
2020-09-02 00:10:42 -0300

    Mensagem de Boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,10 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Bom  dia!");
 		System.out.println("Boa tarde!");
-
+		System.out.println("Boa noite!");
+	
+		for (int i = 0; i<10;i++) {
+			System.out.println();
+		}
 	}
-
 }
